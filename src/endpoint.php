218940f001ed0ae8b84c4727a1f19f5a128e335b
A J <noreply@syzmic.org>
2013-07-03 01:39:40 -0300

    also trim query string  On branch local 	modified:   endpoint.php

--- src/endpoint.php
+++ src/endpoint.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require_once('DiasporaWalker.php');
 require_once('ResultTree.php');
 
@@ -9,10 +8,10 @@ header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
-// browser backspace and history navigation to no work, not sure why
+
 
 $url =  empty($_GET['startUrl']) ? null : $_GET['startUrl'] ;
-$url = $url . ".json" ;
+$url = trim($url . ".json") ;
 // $url = 'https://joindiaspora.com/posts/2772581';
 
 if ($url !== null) {
